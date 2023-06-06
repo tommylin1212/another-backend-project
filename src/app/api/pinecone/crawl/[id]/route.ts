229d3a3b28a81f62a9fd6ea2af3ad78a2e2a9f93
Caleb Tomlin <caleb.w.tomlin@gmail.com>
2023-06-06 12:36:00 -0500

    maybe some more logging

--- src/app/api/pinecone/crawl/[id]/route.ts
+++ src/app/api/pinecone/crawl/[id]/route.ts
@@ -1,11 +1,14 @@
-import { NextResponse } from "next/server"
 import { crawlSpotifyTracks } from "@/spotify/operations"
 import { getSpotifyAccessToken } from "@/spotify/operations"
 
-
 export async function POST(request: Request,{params}: {params: { id: string }}) {
-    const body = await request.json()
-    const { userId } = body
-    crawlSpotifyTracks(params.id, 10, await getSpotifyAccessToken(userId))
-    return new Response('POST request', { status: 200 })
+    try {
+        const body = await request.json()
+        const { userId } = body
+        crawlSpotifyTracks(params.id, 10, await getSpotifyAccessToken(userId))
+        return new Response('POST request', { status: 200 })
+    } catch (error: unknown) {
+        console.error(error);
+        return new Response(`Invalid JSON input: ${error}`, { status: 400 });
+    }
 }
